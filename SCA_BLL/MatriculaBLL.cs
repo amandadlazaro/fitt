53e2f3f57395996e532f669098d6b0adc7c9ca05
kazzuyak <kazzuyak@gmail.com>
2018-11-02 01:07:29 -0300

    add MatriculaBLL and PagamentoBLL

---
+++ SCA_BLL/MatriculaBLL.cs
@@ -0,0 +1,96 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCA_BLL
+{
+    public class MatriculaBLL
+    {
+        #region Propriedades
+        public class DadosMatricula
+        {
+            public int idMatricula { get; set; }
+            public string CPF { get; set; }
+            public int idTurma { get; set; }
+            public string TipoPlano { get; set; }
+            public double ValorMensal { get; set; }
+            public System.DateTime DataInicio { get; set; }
+            public System.DateTime DataFim { get; set; }
+            public bool SituacaoMatricula { get; set; }
+            public int QtdeAulas { get; set; }
+        }
+        #endregion
+
+        public FittDataBaseEntities bd = new FittDataBaseEntities();
+
+
+        public IEnumerable<DadosMatricula> LerMatricula()
+        {
+            var lista = bd.Matricula.Select(p => new DadosMatricula
+            {
+                idMatricula = p.idMatricula,
+                CPF = p.CPF,
+                idTurma = p.idTurma,
+                TipoPlano = p.TipoPlano,
+                ValorMensal = p.ValorMensal,
+                DataInicio = p.DataInicio,
+                DataFim = p.DataFim,
+                SituacaoMatricula = p.SituacaoMatricula,
+                QtdeAulas = p.QtdeAulas
+            }).ToList();
+
+            return lista;
+        }
+
+
+        public string Adicionar(Matricula matricula)
+        {
+            try
+            {
+                bd.Matricula.Add(matricula);
+                bd.SaveChanges();
+                return "Matricula Cadastrada com Sucesso";
+            }
+            catch (Exception error)
+            {
+                return error.Message;
+            }
+        }
+
+        public string Alterar(Matricula matricula)
+        {
+            try
+            {
+                var t = bd.Matricula.First(tm => tm.idMatricula == matricula.idMatricula);
+                if (t == null) return null;
+                bd.Entry(t).CurrentValues.SetValues(matricula);
+                bd.SaveChanges();
+                return "Matricula Alterada com Sucesso";
+            }
+            catch (Exception error)
+            {
+                return error.Message;
+            }
+
+        }
+
+        public string Deletar(Matricula matricula)
+        {
+            try
+            {
+                var t = bd.Matricula.First(tm => tm.idMatricula == matricula.idMatricula);
+                if (t == null) return null;
+                bd.Matricula.Remove(t);
+                bd.SaveChanges();
+                return "Matricula Deletada com Sucesso";
+            }
+            catch (Exception error)
+            {
+                return error.Message;
+            }
+        }
+    }
+}
---
+++ SCA_BLL/PagamentoBLL.cs
@@ -0,0 +1,94 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCA_BLL
+{
+    public class PagamentoBLL
+    {
+        #region Propriedades
+        public class DadosBoleto
+        {
+            public int idBoleto { get; set; }
+            public int idMatricula { get; set; }
+            public string FormaDePagamento { get; set; }
+            public DateTime DtVencimento { get; set; }
+            public Nullable<double> Multa { get; set; }
+            public Nullable<double> Desconto { get; set; }
+            public double ValorTotal { get; set; }
+            public DateTime DtPagamento { get; set; }
+        }
+        #endregion
+
+        public FittDataBaseEntities bd = new FittDataBaseEntities();
+
+
+        public IEnumerable<DadosBoleto> LerBoleto()
+        {
+            var lista = bd.Boleto.Select(p => new DadosBoleto
+            {
+                idBoleto = p.idBoleto,
+                idMatricula = p.idMatricula,
+                FormaDePagamento = p.FormaDePagamento,
+                DtVencimento = p.DtVencimento,
+                Multa = p.Multa,
+                Desconto = p.Desconto,
+                ValorTotal = p.ValorTotal,
+                DtPagamento = p.DtPagamento
+            }).ToList();
+
+            return lista;
+        }
+
+
+        public string Adicionar(Boleto boleto)
+        {
+            try
+            {
+                bd.Boleto.Add(boleto);
+                bd.SaveChanges();
+                return "Pagamento Cadastrado com Sucesso";
+            }
+            catch (Exception error)
+            {
+                return error.Message;
+            }
+        }
+
+        public string Alterar(Boleto boleto)
+        {
+            try
+            {
+                var t = bd.Boleto.First(tm => tm.idBoleto == boleto.idBoleto);
+                if (t == null) return null;
+                bd.Entry(t).CurrentValues.SetValues(boleto);
+                bd.SaveChanges();
+                return "Pagamento Alterado com Sucesso";
+            }
+            catch (Exception error)
+            {
+                return error.Message;
+            }
+
+        }
+
+        public string Deletar(Boleto boleto)
+        {
+            try
+            {
+                var t = bd.Boleto.First(tm => tm.idBoleto == boleto.idBoleto);
+                if (t == null) return null;
+                bd.Boleto.Remove(t);
+                bd.SaveChanges();
+                return "Pagamento Deletado com Sucesso";
+            }
+            catch (Exception error)
+            {
+                return error.Message;
+            }
+        }
+    }
+}

78a44b4999193d5005f4f873906f3bb12fbcae01
amandadlazaro <amanda_dlazaro@outlook.com>
2018-11-08 19:30:59 -0200

    Retirar multa e data vencimento de Boleto DAL

--- DAL/FittEntity.Designer.cs
+++ DAL/FittEntity.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\amand\Documents\git\fitt\DAL\FittEntity.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\amanda.lima\source\repos\fitt\DAL\FittEntity.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.

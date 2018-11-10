26cda3580de8a73a2fac2f8a0b1c0a48f4ece959
kazzuyak <kazzuyak@gmail.com>
2018-11-10 01:10:31 -0200

    frmGerarpag

--- SCA/FrmGerarPagamento.cs
+++ SCA/FrmGerarPagamento.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace SCA
+namespace FittSistema.View
 {
     public partial class FrmGerarPagamento : Form
     {
